f43c7608f16d7469278a60f8276105c357d5aa60
Nicholas Nelson <nicholasmnelson10@gmail.com>
2024-09-21 12:19:00 -0700

    Initial Commit

---
+++ light/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  light
+//
+//  Created by Nicholas Nelson on 9/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ light/lightApp.swift
@@ -0,0 +1,17 @@
+//
+//  lightApp.swift
+//  light
+//
+//  Created by Nicholas Nelson on 9/21/24.
+//
+
+import SwiftUI
+
+@main
+struct lightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
